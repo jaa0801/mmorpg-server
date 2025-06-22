cb39a82537c0ce0014e657d3277f8fe7a97bc965
jaa0801 <virtualis003@gmail.com>
2025-06-22 23:07:43 +0800

    🔧 Fix: Updated Colyseus Player schema with @type fields

--- src/rooms/schema/MyRoomState.ts
+++ src/rooms/schema/MyRoomState.ts
@@ -1,22 +1,16 @@
 import { Schema, type, MapSchema } from "@colyseus/schema";
 
 export class Player extends Schema {
-  @type("number") x = 100;
-  @type("number") y = 100;
+  @type("string") name = "";
+  @type("number") x = 0;
+  @type("number") y = 0;
 
   @type("string") animation = "";
-
   @type("string") imageIdleFront = "";
   @type("string") imageWalkLeft = "";
   @type("string") imageWalkRight = "";
   @type("string") imageWalkUp = "";
   @type("string") imageWalkDown = "";
-
-  // Optional properties
-  @type("string") name = "";
-  @type("number") level = 1;
-  @type("number") hp = 100;
-  @type("string") class = "";
 }
 
 export class MyRoomState extends Schema {
